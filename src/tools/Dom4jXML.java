04f2584325b52a8f279ef0630fb3f36e3fefdd13
Serana <angeltsh@foxmail.com>
2015-06-06 09:36:40 +0800

    生成XML

--- src/tools/Dom4jXML.java
+++ src/tools/Dom4jXML.java
@@ -11,34 +11,67 @@ import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
 public class Dom4jXML {
-	public int createXMLFile(String filename) {
 
+	public static int createXMLFile(String filename) {
 		int returnValue = 0;
 
 		Document document = DocumentHelper.createDocument();
-		Element booksElement = document.addElement("books");
-		booksElement.addComment("This is a test for dom4j, holen, 2004.9.11");
-		Element bookElement = booksElement.addElement("book");
-		/** 加入show属性内容 */
-		bookElement.addAttribute("show", "yes");
-		/** 加入title节点 */
-		Element titleElement = bookElement.addElement("title");
-		/** 为title设置内容 */
-		titleElement.setText("Dom4j Tutorials");
+		document.addComment("订单详细内容");
 
-		/** 类似的完成后两个book */
-		bookElement = booksElement.addElement("book");
-		bookElement.addAttribute("show", "yes");
-		titleElement = bookElement.addElement("title");
-		titleElement.setText("Lucene Studing");
-		bookElement = booksElement.addElement("book");
-		bookElement.addAttribute("show", "no");
-		titleElement = bookElement.addElement("title");
-		titleElement.setText("Lucene in Action");
+		Element root = document.addElement("OrderData");
+		root.addAttribute("id", "keyId");
 
-		/** 加入owner节点 */
-		Element ownerElement = booksElement.addElement("owner");
-		ownerElement.setText("O'Reilly");
+		// 订单编号
+		root.addComment("订单编号");
+		Element orderNo = root.addElement("OrderNo");
+		orderNo.setText("TN123456789");
+
+		// 订单金额
+		root.addComment("订单价格");
+		Element price = root.addElement("Price");
+		price.addAttribute("unit", "￥");
+		price.setText("50.00");
+
+		root.addComment("左手拇指");
+		Element imgL1 = root.addElement("imgL1");
+		imgL1.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
+		root.addComment("左手食指");
+		Element imgL2 = root.addElement("imgL2");
+		imgL2.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
+		root.addComment("左手中指");
+		Element imgL3 = root.addElement("imgL3");
+		imgL3.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
+		root.addComment("左手无名指");
+		Element imgL4 = root.addElement("imgL4");
+		imgL4.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
+		root.addComment("左手小指");
+		Element imgL5 = root.addElement("imgL5");
+		imgL5.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
+		root.addComment("右手拇指");
+		Element imgR1 = root.addElement("imgR1");
+		imgR1.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
+		root.addComment("右手食指");
+		Element imgR2 = root.addElement("imgR2");
+		imgR2.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
+		root.addComment("右手中指");
+		Element imgR3 = root.addElement("imgR3");
+		imgR3.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
+		root.addComment("右手无名指");
+		Element imgR4 = root.addElement("imgR4");
+		imgR4.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
+		root.addComment("右手小指");
+		Element imgR5 = root.addElement("imgR5");
+		imgR5.setText("http://192.168.1.136:8080/3dmeijia/images/app-nails/test.jpg");
+
 		try {
 			/** 将document中的内容写入文件中 */
 			XMLWriter writer = new XMLWriter(new FileWriter(new File(filename)));
@@ -49,6 +82,9 @@ public class Dom4jXML {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+
+		formatXMLFile(filename);
+
 		return returnValue;
 	}
 
@@ -58,7 +94,7 @@ public class Dom4jXML {
 	 * @param filename
 	 * @return
 	 */
-	public int formatXMLFile(String filename) {
+	private static int formatXMLFile(String filename) {
 		int returnValue = 0;
 		try {
 			SAXReader saxReader = new SAXReader();
@@ -67,7 +103,7 @@ public class Dom4jXML {
 			/** 格式化输出,类型IE浏览一样 */
 			OutputFormat format = OutputFormat.createPrettyPrint();
 			/** 指定XML编码 */
-			format.setEncoding("GBK");
+			format.setEncoding("UTF-8");
 			writer = new XMLWriter(new FileWriter(new File(filename)), format);
 			writer.write(document);
 			writer.close();
@@ -83,9 +119,6 @@ public class Dom4jXML {
 	}
 
 	public static void main(String[] args) {
-
-		Dom4jXML temp = new Dom4jXML();
-		System.out.println(temp.createXMLFile("g://holen.xml"));
-		System.out.println(temp.formatXMLFile("g://holen.xml"));
+		Dom4jXML.createXMLFile("g://holen.xml");
 	}
 }
