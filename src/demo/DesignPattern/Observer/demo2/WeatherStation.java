c2ffc3451cf90064a6d192a01b7b2a1e0bd0caef
angeltsh <ywy0719@163.com>
2015-10-15 14:58:11 +0800

    观察者

--- src/demo/DesignPattern/Observer/demo2/WeatherStation.java
+++ src/demo/DesignPattern/Observer/demo2/WeatherStation.java
@@ -10,14 +10,26 @@ public class WeatherStation {
 
 		DisplayCurrentConditions currentConditions = new DisplayCurrentConditions(
 				weatherData);
+		DisplayForecast forecast = new DisplayForecast(weatherData);
+		DisplayStatistics statistics = new DisplayStatistics(weatherData);
 
+		System.out.println("=======第一次更新=======");
 		weatherData.setMeasurements(80, 65, 30.4f);
 		currentConditions.update(weatherData, null);
+		forecast.update(weatherData, null);
+		statistics.update(weatherData, null);
 
+		System.out.println("=======第二次更新=======");
 		weatherData.setMeasurements(82, 70, 29.2f);
+		currentConditions.update(weatherData, null);
+		forecast.update(weatherData, null);
+		statistics.update(weatherData, null);
+
+		System.out.println("=======第三次更新=======");
 		weatherData.setMeasurements(78, 90, 29.2f);
 		currentConditions.update(weatherData, null);
-
+		forecast.update(weatherData, null);
+		statistics.update(weatherData, null);
 	}
 
 }
