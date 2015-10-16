02eb2ad6adb4ea113b50625ba4d3d04b92e7d07e
angeltsh <ywy0719@163.com>
2015-10-16 11:16:40 +0800

    转移设计模式

--- src/demo/DesignPattern/Decorator/DecoratorCondiment.java
+++
@@ -1,5 +0,0 @@
-package demo.DesignPattern.Decorator;
-
-public abstract class DecoratorCondiment extends Beverage {
-	public abstract String getDescription();
-}
--- src/demo/DesignPattern/Observer/demo1/DisplayElement.java
+++
@@ -1,9 +0,0 @@
-package demo.DesignPattern.Observer.demo1;
-
-/**
- * Õ¹Ê¾°æ
- */
-public interface DisplayElement {
-
-	void display();
-}
